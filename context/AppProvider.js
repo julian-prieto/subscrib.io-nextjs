62768cd26c9e1ebb376f8acd173637de69e6a872
Julián Prieto <herbie.juls@gmail.com>
2022-03-14 10:26:06 -0300

    Add System Theme for default theme

--- context/AppProvider.js
+++ context/AppProvider.js
@@ -3,7 +3,7 @@ import { useStateWithStorage } from "hooks";
 import { AppContext } from "context/AppContext";
 
 export const AppProvider = ({ children }) => {
-  const [storageTheme, setStorageTheme] = useStateWithStorage("theme", "dark");
+  const [storageTheme, setStorageTheme] = useStateWithStorage("theme");
   const [theme, setTheme] = useState("light");
   const [user, setUser] = useState(null);
 
@@ -17,8 +17,33 @@ export const AppProvider = ({ children }) => {
   const setUserOnAuthChange = (user) => setUser(user);
 
   useEffect(() => {
-    setTheme(storageTheme);
+    if (storageTheme) {
+      setTheme(storageTheme);
+    }
   }, []);
+
+  useEffect(() => {
+    const colorSchema = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleSystemThemeChange = (event) => {
+      const newColorScheme = event.matches ? "dark" : "light";
+      if (!storageTheme) {
+        setTheme(newColorScheme);
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      const systemTheme = colorSchema.matches ? "dark" : "light";
+      setTheme(storageTheme ? storageTheme : systemTheme);
+      colorSchema.addEventListener("change", handleSystemThemeChange);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        colorSchema.removeEventListener("change", handleSystemThemeChange);
+      }
+    };
+  }, [storageTheme]);
 
   return (
     <AppContext.Provider
